57a182b4d4a7c38626eb346a8d93be159ed1938a
M. Amalan <amalan0595@gmail.com>
2019-01-26 14:59:00 +0530

    made new changes to the shiny with tabs and theme

--- Olympic/server.R
+++ Olympic/server.R
@@ -1,82 +1,82 @@
 # load the packages
 library(shiny)
 library(magrittr)
-library(readr)
 library(DT)
 library(summarytools)
-library(shiny)
 library(tidyverse)
 library(htmlTable)
-library(readr)
 library(htmlwidgets)
 
 # load the data
 load("Data/data.RData")
-attach(Olympic)
 
 function(input, output){
+    # NOC and codes for data
+    output$data<-DT::renderDataTable({
+                                      datatable(NOC,options = list(pageLength = 30))
+                                    })
+  
     # creating the plots
     output$plot <- renderPlot({
     
                               Medal_color<-c("#CD7F32","#FFD700","#C0C0C0")
-                              names(Medal_color)<-levels(factor(Medal))
+                              names(Medal_color)<-levels(factor(Olympic$Medal))
                               McolScale<-scale_color_manual(name="Medal",values=Medal_color,na.value="red")
                               
                               if(input$NoNA == FALSE)
                               {
                               subset(Olympic,NOC==input$NOC) %>%
                               ggplot(.,aes(x=Season,y=Sex,color=factor(Medal)))+
-                              geom_jitter()+facet_wrap(~factor(Year))+
+                              geom_jitter()+facet_wrap(~factor(Year),ncol = 4)+
                               xlab("Years of Participation")+
-                              ylab("Gender")+ McolScale
+                              ylab("Gender")+ McolScale+
+                              ggtitle("MEDALS WON OVER THE YEARS ACCORDING TO GENDER")
                               }
                               else
                               {
                               subset(Olympic[!is.na(Olympic$Medal),],NOC==input$NOC) %>%
                               ggplot(.,aes(x=Season,y=Sex,color=factor(Medal)))+
-                              geom_jitter()+facet_wrap(~factor(Year))+
+                              geom_jitter()+facet_wrap(~factor(Year),ncol=4)+
                               xlab("Years of Participation")+
-                              ylab("Gender")+
-                              McolScale
+                              ylab("Gender")+McolScale+
+                              ggtitle("MEDALS WON OVER THE YEARS ACCORDING TO GENDER")
                               }
-                               },height = 1000,width = 1200)  
+                               },height = 1400,width = 1200)  
                               
   # creating the table
   output$table<-DT::renderDataTable({
                                    datatable(subset(Olympic,NOC==input$NOC, 
-                                                    select = -c(Height,Weight,Team,NOC,Games)),
-                                             options = list(pageLength= 25))
+                                                    select = -c(Height,Weight,Team,NOC,Games,City,Sport)),
+                                             options = list(pageLength= 30))
                                    })
   # creating the summary for selected variables
   output$summary<-renderUI({
                             print(dfSummary(subset(Olympic,NOC==input$NOC, 
-                                                   select =c(Sex,Age,Year,Season,Sport,Medal))
+                                                   select =c(Sex,Age,Height,Weight,Year,Season,Sport,Medal))
                                             ),method = 'render',headings = FALSE, bootstracp.css = FALSE)
                           })
-  # NOC and codes for data
-  output$data<-DT::renderDataTable({
-                                  datatable(NOC,options = list(pageLength = 25))
-                                  })
+
   # Gender Count over the year
   output$barplot <- renderPlot({
                               Gender_color<-c("#DEA1C0","#6BA5DE")
-                              names(Gender_color)<-levels(factor(Sex))
-                              GcolScale<-scale_color_manual(name="Sex",values=Gender_color)
+                              names(Gender_color)<-levels(factor(Olympic$Sex))
+                              GcolScale<-scale_color_manual(name="Gender",values=Gender_color)
                               
                               subset(Olympic,NOC==input$NOC,select = c(Sex,Year,Season)) %>%
                               ggplot(.,aes(x=factor(Year),fill=factor(Sex),group=factor(Sex)))+
                               geom_bar(position = "dodge")+
                               xlab("Years")+ylab("Frequency")+coord_flip()+ GcolScale+
                               labs(fill="Gender")+
+                              ggtitle("GENDER RELATED PARTICIPATION OVER THE YEARS")+
                               geom_text(stat='count',aes(y=..count..,label=..count..),
-                                        position=position_dodge(width = 1),hjust=1,
+                                        position=position_dodge(width = 1),hjust=1.25,
                                         color="#696969",size=4)
-                              },height = 1200,width = 1200)
-  # Gender Count for Events
+                              },height = 1400,width = 1200)
+  # Gender Count for Sports
   output$SportsBarplot<-renderPlot({
                                     Gender_color<-c("#DEA1C0","#6BA5DE")
-                                    names(Gender_color)<-levels(factor(Sex))
-                                    GcolScale<-scale_color_manual(name="Sex",values=Gender_color)
+                                    names(Gender_color)<-levels(factor(Olympic$Sex))
+                                    GcolScale<-scale_color_manual(name="Gender",values=Gender_color)
                                     
                                     subset(Olympic,NOC==input$NOC,select = c(Sex,Sport,Season)) %>%
                                     ggplot(.,aes(x=fct_infreq(factor(Sport)),fill=factor(Sex),
@@ -84,10 +84,23 @@ function(input, output){
                                     geom_bar(position = "dodge")+
                                     xlab("Sport")+ylab("Frequency")+coord_flip()+
                                     labs(fill="Gender")+GcolScale+
+                                    ggtitle("GENDER REPRESENTATION IN SPORTS OF PARTICIPANTS")+
                                     geom_text(stat='count',aes(y=..count..,label=..count..),
-                                              position=position_dodge(width = 1),hjust=1,
+                                              position=position_dodge(width = 1),hjust=1.25,
                                               color="#696969",size=3)
      
-                                  },height = 1200,width = 1200)
+                                  },height = 1400,width = 1200)
+  # Scatter plot for height, Sports and Years with gender
+  output$HWSplot<-renderPlot({
+                               Gender_color<-c("#DEA1C0","#6BA5DE")
+                               names(Gender_color)<-levels(factor(Olympic$Sex))
+                               GcolScale<-scale_color_manual(name="Gender",values=Gender_color)
+                               
+                               subset(Olympic,NOC==input$NOC,select = c(Height,Weight,Sport,Sex)) %>%
+                               ggplot(.,aes(x=Weight,y=Height,color=factor(Sex)))+geom_point()+
+                               ggtitle("HEIGHT AND WEIGHT OF PARTICIPANTS BASED ON SPORTS")+
+                               labs(color="Gender")+xlab("Weight (kg)")+ylab("Height (cm)")+
+                               facet_wrap(~Sport,ncol = 4)+GcolScale
+                             },height = 1400,width = 1200)
   
                         }
--- Olympic/ui.R
+++ Olympic/ui.R
@@ -1,46 +1,65 @@
+library(shinythemes)
+
 
 # load the data
 load("Data/data.RData")
 
 # starting the rshiny ui
-fluidPage(
+fluidPage(theme = shinytheme("flatly"),
   
           # Application title
-          titlePanel("OLYMPIC DATA FROM KAGGLE : AN RSHINY PERSPECTIVE"),
+          titlePanel("OLYMPIC DATA FROM KAGGLE : AN RSHINY PERSPECTIVE",windowTitle = "Olympic : Rshiny"),
   
           # sidebar which incldues the image and title, information
           sidebarLayout(
                         sidebarPanel(
                                      h3("Olympic & Countries From 1896 Until 2016",align="center"),
+                                     br(),
                                      img(src='Logo.png', align= "center",height='75%',width='95%'),
-                                     h4("How to Navigate the Olympic Rshiny App"),
-                                     helpText("1. Choose your country which is represented by a three letter code"),
+                                     br(),
+                                     h4("How to Navigate the Olympic Rshiny App",align="center"),
+                                     br(),
+                                     h5("1. Choose your country which is represented by a three letter code"),
                                      selectInput('NOC',"Choose Your Country:",
                                                  choices = sort(unique(Olympic$NOC)),
-                                                 selected = "SRI",selectize = TRUE),
+                                                 selected = "SRI",selectize = TRUE,width='200px'),
                                      helpText(" For Example the code SRI is the representation to Sri Lanka"),
-                                     helpText("2. If you do not know the Code use the *NOC CODE* tab."),
-                                     helpText("3. Click on *GRAPH* tab to see how the medals were won 
+                                     br(),
+                                     h5("2. If you do not know the Code use the *NOC CODE* tab."),
+                                     br(),
+                                     h5("3. Click on *MEDAL GRAPH* tab to see how the medals were won 
                                                   for each year for the chosen country with respective to Gender."),
                                      checkboxInput("NoNA", label = "Remove NA", value = FALSE),
-                                     helpText("Choose if you want to to remove the NA values from the plot in *GRAPH* tab."),
-                                     helpText("4. Show the data of the chosen country using *DATA* tab."),
-                                     helpText("5. Print a Summary Table of the chosen country using the *DESCRIBE* tab."),
-                                     helpText("6. Use *G/Years* tab to understand how Gender representation has changed over
+                                     helpText("Choose if you want to to remove the NA values from the plot in *MEDAL GRAPH* tab."),
+                                     br(),
+                                     h5("4. Show the data of the chosen country using *DATA* tab."),
+                                     br(),
+                                     h5("5. Print a Summary Table of the chosen country using the *DESCRIBE* tab."),
+                                     br(),
+                                     h5("6. Use *G/Years* tab to understand how Gender representation has changed over
                                                   the years of participation for the chosen country."),
-                                     helpText("7. Finally Use *S/Years* to understand how Gender representation has changed over
+                                     br(),
+                                     h5("7. Use *S/Years* to understand how Gender representation has changed over
                                                   sports participated by the attendees for the chosen country."),
-                                     helpText("* Do the above steps for different countries and observe them for your amusement.")
+                                     br(),
+                                     h5("8. Finally, Use the *H/W/Sport* tab to explore how participants
+                                                 Height and Weight relationship for each Sporting event with respective
+                                                 to gender for the chosen country."),
+                                     br(),
+                                     helpText("*Do the above steps for different countries and observe them for your amusement.*"),
+                                     br(),
+                                     h4("Thank You",align="center")
                                     ),
           # tabs for 4 types
           mainPanel(
                     tabsetPanel(type="tabs",
                                 tabPanel("NOC CODE",DT::dataTableOutput("data")),
-                                tabPanel("GRAPH",plotOutput("plot")),
+                                tabPanel("MEDAL GRAPH",plotOutput("plot")),
                                 tabPanel("DATA",DT::dataTableOutput("table")), 
                                 tabPanel("DESCRIBE",htmlOutput("summary")),
                                 tabPanel("G/Years",plotOutput("barplot")),
-                                tabPanel("S/Years",plotOutput("SportsBarplot"))
+                                tabPanel("S/Years",plotOutput("SportsBarplot")),
+                                tabPanel("H/W/Sport",plotOutput("HWSplot"))
                                ) 
                   )
                       ,fluid = FALSE)
